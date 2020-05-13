9fd8367e92cc1bcbc993dfa8a443737375c9e641
dhkim1211 <dhkim1211@gmail.com>
2020-05-13 16:04:21 -0700

    set up react + amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes-app-dk-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://27mbracgsc.execute-api.us-east-2.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_d9BgT7Hp9",
+    APP_CLIENT_ID: "5hatarbfnse8a78is2ulqvibui",
+    IDENTITY_POOL_ID: "us-east-2:cf4aaa5d-b3cf-4530-9097-106bec51df27"
+  }
+};
